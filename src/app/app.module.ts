20a416c221ee0758e2427233c218e96d07b8f935
AyhamZataimeh <ayham.zataimeh99@gmail.com>
2023-12-17 23:28:20 +0200

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { MasterComponent } from './components/master/master.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MasterComponent
   ],
   imports: [
     BrowserModule
